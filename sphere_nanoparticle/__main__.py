d791faf69fb26b6d4f4b73dac093ffecc1e162b5
Nikita Malikov <malikovnmz@gmail.com>
2020-02-19 18:35:52 +0500

    write modes in file

--- sphere_nanoparticle/__main__.py
+++ sphere_nanoparticle/__main__.py
@@ -1,10 +1,18 @@
 import initial_values as iv
 import custom_materials as cm
 import sim
+import datetime
 
 
-def mote_output(modes):
-    return 0
+def modes_output(modes, material):
+    def list_to_str(modes):
+        return '\nDate:   ' + datetime.datetime.now().__str__() + '   Material:   ' \
+               + material + '\n' + modes.__str__()
+
+    def op():
+        return open('sphere_nanoparticle/output/modes', 'a')
+
+    return op().write(list_to_str(modes))
 
 
 def arg():
@@ -12,10 +20,11 @@ def arg():
 
 
 if arg().mod == 'harm':
-    sim.mod(
+    modes_output(sim.mod(
         sim.create(arg().res, arg().rad, cm.material_lib_dict(arg().mat), arg().wl, arg().wid, arg().rem, arg().pml),
         arg().rem,
         arg().rad,
         arg().wl,
         arg().wid,
-        arg().t)
+        arg().t),
+        arg().mat)
